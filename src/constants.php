feb0077f57c814d957e083907cb4239650fecc3e
Damon Smith <damon.easy.system@gmail.com>
2016-06-21 22:00:00 +0300

    add constants.php

---
+++ src/constants.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the "Easy System" package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Damon Smith <damon.easy.system@gmail.com>
+ */
+namespace Es;
+
+define('__ES__', 'Easy System');
+
+define('DS', DIRECTORY_SEPARATOR);
+
+define('PHP_INT_MIN', ~PHP_INT_MAX);
